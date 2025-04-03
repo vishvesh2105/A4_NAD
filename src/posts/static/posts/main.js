2bd02b235b5a603b6168a491a5dc2dda2005a3c5
Vishvesh Lakhani <vishweshlakhani456@gmail.com>
2025-04-03 01:06:35 -0400

    Created more files while going ahead in the videos also implemented ajax query and main.js

---
+++ src/posts/static/posts/main.js
@@ -0,0 +1,11 @@
+console.log('hello world')
+
+const hellowolrdBox = document.getElementById('hello-world')
+
+hellowolrdBox.innerHTML = "hello <b>world"
+
+s.ajax({
+    type: 'GET',
+    url: '/hello-world',
+
+})
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import post_list_and_create
+from .views import hello_world_view
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+
+    path ('hello-world/', hello_world_view, name='hello-world'),
 ]
