575e313afbb77413ea5211a62bde2b1005459eed
Vishvesh Lakhani <vishweshlakhani456@gmail.com>
2025-04-03 03:30:11 -0400

    Done with practicing Spinners.

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,6 +1,8 @@
 console.log('hello world')
 
 const hellowolrdBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
@@ -11,5 +13,29 @@ $.ajax({
     },
     error: function(error) {
         console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b> <br>
+                `
+            });
+
+        }, 1000)
+
+     },
+    
+    error:function(error){
+        console.log(error)
     }
 })
