b57f53a376c8d0765ad26753fa2bdd82477fff83
Vishvesh Lakhani <vishweshlakhani456@gmail.com>
2025-04-03 16:32:25 -0400

    Build the basic of Like and Unlike button where we can edit from the admin acces for the basic one and in next I will implement more logic behind it

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -19,21 +19,25 @@ $.ajax({
             console.log(data)
             data.forEach(el => {
                 postsBox.innerHTML += `
-                <div class="card mb-2">
-                     <div class="card-body">
-                     <h5 class="card-title">${el.title}</h5>
-                     <p class="card-text">${el.body}</p>
-                     </div>
-                     <div class="card-footer">
-                     <div class="row">
-                        <div class="col-1">
-                            <a href="#" class="btn btn-primary">Details</a>
-                     </div>
-                     <div class="col-1">
-                     <a href="#" class="btn btn-primary">Like</a>
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-2">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-2">
+                                    <a href="#" class="btn btn-primary">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 `
-            });
+            })
+            
 
         }, 100)
         console.log(response.size)
