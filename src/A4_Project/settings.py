0c19da7d384fadee9f370a68c9c9a03d74918f3f
Vishvesh Lakhani <vishweshlakhani456@gmail.com>
2025-04-02 23:24:28 -0400

    I have added the Post section in the admin UI

--- src/A4_Project/settings.py
+++ src/A4_Project/settings.py
@@ -121,13 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS ={
+STATICFILES_DIRS =[
     BASE_DIR / 'static',
     BASE_DIR / 'posts' / 'static',
     BASE_DIR / 'profiles' / 'static',
-}
+]
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from profiles.models. import Profile
+from profiles.models import Profile
 # Create your models here.
 
 class Post(models.Model):
