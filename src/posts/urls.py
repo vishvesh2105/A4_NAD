0d1cf6ff7b35c792589cdc123af5806f8dfc2c09
Vishvesh Lakhani <vishweshlakhani456@gmail.com>
2025-04-04 02:41:31 -0400

    I have implemented the Detail button logic which actually shows detail of the post

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,13 +3,15 @@ from .views import post_list_and_create
 from .views import hello_world_view
 from .views import load_post_data_view
 from .views import like_unlike_post
+from .views import post_detail
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
+    path('<pk>', post_detail, name='post-detail'),
+
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
 
-    path ('hello-world/', hello_world_view, name='hello-world'),
 ]
