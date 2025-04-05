0d9d4df91c8af01cce37809aa5b3509839860a82
Vishvesh Lakhani <vishweshlakhani456@gmail.com>
2025-04-05 01:00:27 -0400

    I have completed the Update Post with Ajax which allows to edit post.

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,13 +1,23 @@
 console.log('Hello world')
 
 const postBox = document.getElementById('post-box')
+const alertBox = document.getElementById('alert-box')
 const backBtn = document.getElementById('back-btn')
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
-const url = window.location.href.endsWith('/') 
+const url = window.location.href.endsWith('/')
     ? window.location.href + 'data/' 
     : window.location.href + '/data/';
 const spinnerBox = document.getElementById('spinner-box')
+const updateUrl = window.location.href.endsWith('/')
+    ? window.location.href + 'update/' 
+    : window.location.href + '/update/';
+const deleteUrl = window.location.href.endsWith('/')
+    ? window.location.href + 'delete/' 
+    : window.location.href + '/delete/'
+
+const updateForm = document.getElementById('update-form')
+const deleteForm = document.getElementById('delete-form')
 
 const titleInput = document.getElementById('id_title')
 const bodyInput = document.getElementById('id_body')
@@ -33,9 +43,11 @@ $.ajax({
 
         const titleEl = document.createElement('h3')
         titleEl.setAttribute('class', 'mt-3')
+        titleEl.setAttribute('id', 'title')
 
         const bodyEl = document.createElement('p')
         bodyEl.setAttribute('class', 'mt-1')
+        bodyEl.setAttribute('id', 'body')
 
         titleEl.textContent = data.title
         bodyEl.textContent = data.body
@@ -51,4 +63,48 @@ $.ajax({
     error: function(error){
         console.log(error);
     }  
+})
+
+
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+updateForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    const title = document.getElementById('title')
+    const body = document.getElementById('body')
+
+    $.ajax({
+        type: 'POST',
+        url: updateUrl,
+        data:{
+            'csrfmiddlewaretoken': csrftoken,
+            'title': titleInput.value,
+            'body': bodyInput.value,
+        },
+        success: function(response){
+            console.log(response)
+            handleAlerts('success', 'Post has been updated.')
+            title.textContent = response.title
+            body.textContent = response.body
+        },
+        error: function(error){
+            console.log(error)
+        }
+
+    })
 })
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -4,6 +4,10 @@ from .views import load_post_data_view
 from .views import like_unlike_post
 from .views import post_detail_data_view
 from .views import post_detail
+from .views import delete_post
+from .views import update_post
+
+
 
 app_name = 'posts'
 
@@ -11,6 +15,8 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>', post_detail, name='post-detail'),
+    path('<pk>/update/', update_post, name='post-update'),
+    path('<pk>/delete/', delete_post, name='post-delete'),
 
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
